f27760d73817cfffe550b655b4b66727758b9f66
Adam <adam1092002@icloud.com>
2024-04-12 21:59:12 +0100

    Add Villa & Villas Get

---
+++ Villa_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Villa_VillaAPI.Data;
+using Villa_VillaAPI.Models;
+using Villa_VillaAPI.Models.DTO;
+
+namespace Villa_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas() 
+        {
+            return Ok(VillaStore.villaList);
+        }
+
+        [HttpGet("id:int")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
+        }
+
+
+    }
+}
---
+++ Villa_VillaAPI/Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace Villa_VillaAPI.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
